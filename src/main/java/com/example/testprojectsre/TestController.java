4d536abd68785ebcd88c7ad8f1594e9907281934
gkiss <gergely@20shots.co.uk>
2021-12-01 17:56:21 +0000

    jdbc application

--- src/main/java/com/example/testprojectsre/TestController.java
+++ src/main/java/com/example/testprojectsre/TestController.java
@@ -1,13 +1,20 @@
 package com.example.testprojectsre;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController
 {
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
     @GetMapping("/test")
     public String test(){
         return "test endpoint called successfully";
+    }
+    @GetMapping("/test-db")
+    public String testDb(){
+        return "test endpoint called successfully: " + datasourceUrl;
     }
 }
