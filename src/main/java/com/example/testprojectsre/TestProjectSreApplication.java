1e69a149556de9a2280f8f4fad1698bafb627140
gkiss <gergely@20shots.co.uk>
2021-12-01 17:28:32 +0000

    Initial commit

---
+++ src/main/java/com/example/testprojectsre/TestProjectSreApplication.java
@@ -0,0 +1,15 @@
+package com.example.testprojectsre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectSreApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TestProjectSreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testprojectsre/TestProjectSreApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.testprojectsre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectSreApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
